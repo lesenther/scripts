3f19bdd8d3f73d4408ccab9bf4de65f1b2e1c948
Logan Esenther <le@berkeley.edu>
2016-08-27 17:01:16 -0700

    Suppress output from screen, fix error level block, fix error from echoing parentheses

--- cycle_ics/cycle_adapter_and_ics.bat
+++ cycle_ics/cycle_adapter_and_ics.bat
@@ -1,19 +1,22 @@
 @echo off
 
+cls
 echo Checking internet...
-ping www.google.com -n 1 -w 1000
+ping www.google.com -n 1 -w 1000 >nul 2>&1
 
-if %errorlevel% neq 0 (
-	echo Internet appears to be down, reseting wireless interface..
-	netsh interface set interface name="Wi-Fi" admin=DISABLED
-	timeout /t 2 /nobreak
-	netsh interface set interface name="Wi-Fi" admin=ENABLED
-	echo Done!  
-	timeout /t 1 /nobreak
-	echo Cycling Internet Connection Sharing (ICS)...
-	powershell -ExecutionPolicy ByPass -Command "$m = New-Object -ComObject HNetCfg.HNetShare; $c = $m.EnumEveryConnection |? { $m.NetConnectionProps.Invoke($_).Name -eq 'Wi-Fi' }; $config = $m.INetSharingConfigurationForINetConnection.Invoke($c); $config.DisableSharing();Start-Sleep -s 5;$config.EnableSharing(0);"
-	echo Done!  	
-) else (
-	echo Internet appears to be working!
-	exit /b
+echo.
+
+if errorlevel 1 (
+  echo Internet appears to be down, reseting wireless interface..
+  netsh interface set interface name="Wi-Fi" admin=DISABLED
+  timeout /t 2 /nobreak >nul
+  netsh interface set interface name="Wi-Fi" admin=ENABLED
+  echo Interface has been successfully reset!
+
+  echo Cycling Internet Connection Sharing...
+  powershell -ExecutionPolicy ByPass -Command "$m = New-Object -ComObject HNetCfg.HNetShare; $c = $m.EnumEveryConnection |? { $m.NetConnectionProps.Invoke($_).Name -eq 'Wi-Fi' }; $config = $m.INetSharingConfigurationForINetConnection.Invoke($c); $config.DisableSharing();Start-Sleep -s 5;$config.EnableSharing(0);"
+  echo Internet Connection Sharing has been successfully reset!
+  exit /b
 )
+
+echo Internet appears to be working!
